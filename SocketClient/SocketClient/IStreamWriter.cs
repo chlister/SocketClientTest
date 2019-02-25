589261862348245c1a7180f5e05dd4bab792e889
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2019-02-25 22:16:18 +0100

    Refactoring code

---
+++ SocketClient/SocketClient/IStreamWriter.cs
@@ -0,0 +1,20 @@
+﻿using SocketClientTest.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient.SocketClient
+{
+    interface IStreamWriter
+    {
+        /// <summary>
+        /// Sending the message object over a given network stream
+        /// </summary>
+        /// <param name="networkStream"><i>NetworkStream</i> of a Socket</param>
+        /// <param name="message"><i>Message</i> object containing who the message is intended and the message</param>
+        void WriteToStream(NetworkStream networkStream, Message message);
+    }
+}
---
+++ SocketClient/SocketClient/SocketClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using SocketClientTest.Client.Models;
+
+namespace SocketClient.SocketClient
+{
+    public class SocketClient : IStreamReader, IStreamWriter
+    {
+        public TcpClient Client { get; private set; }
+        public int Port { get; private set; }
+        public string ServerEndpoint { get; private set; }
+
+
+
+        /// <summary>
+        /// Connects the client to the specified server endpoint and port
+        /// </summary>
+        public void StartClient()
+        {
+            try
+            {
+                Client.Connect(IPAddress.Parse(ServerEndpoint), Port);
+                Console.WriteLine("Client connected to {0}, on port num: {1}", 
+                    ServerEndpoint, Port);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error connecting");
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void WriteToStream(NetworkStream networkStream, Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads a given networkstream
+        /// </summary>
+        /// <param name="networkStream"></param>
+        /// <returns></returns>
+        public byte[] ReadStream(NetworkStream networkStream)
+        {
+            // Define return
+
+            // Only read if client connected
+            if (Client.Connected)
+            {
+
+            }
+            return null;
+        }
+    }
+}
